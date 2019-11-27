3b42f9a20651a12fb50d7905d7342ab457c5aac6
Mário Gil <mg_mesquita@hotmail.com>
2019-11-27 18:14:15 +0000

    Setup scene and rocket

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+    private void ProcessInput() {
+        if(Input.GetKey(KeyCode.Space)) {
+            Debug.Log("Thrust");
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            Debug.Log("Rotating left");
+        }
+        else if(Input.GetKey(KeyCode.D)) {
+            Debug.Log("Rotating right");
+        }
+    }
+}
