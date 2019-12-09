6914e939a9c1a9f82bd3753c30130e3070bebaba
GambuzX <mg_mesquita@hotmail.com>
2019-12-09 23:32:45 +0000

    Level transitions; Sounds

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -1,31 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
 
     [SerializeField] float thrustSpeed = 1f;
     [SerializeField] float rotationSpeed = 1f;
+    [SerializeField] AudioClip mainEngineSound;
+    [SerializeField] AudioClip deathSound;
+    [SerializeField] AudioClip levelClearSound;
 
     private Rigidbody rigidBody;
     private AudioSource audioSource;
+
+    enum State {
+        ALIVE = 0,
+        DYING,
+        TRANSCENDING
+    }
+
+    State state = State.ALIVE;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
-        audioSource = GetComponent<AudioSource>();    
+        audioSource = GetComponent<AudioSource>();
+
+        audioSource.clip = mainEngineSound;
     }
 
     // Update is called once per frame
     void Update()
     {
-        HandleThrust();
-        HandleRotation();
+        if (state == State.ALIVE) {
+            HandleThrust();
+            HandleRotation();
+        }
     }
 
     private void HandleThrust() {
+        
         if(Input.GetKey(KeyCode.Space)) {
             rigidBody.AddRelativeForce(Vector3.up * Time.deltaTime * thrustSpeed);
         }
@@ -50,9 +67,19 @@ public class Rocket : MonoBehaviour
     }
 
     void OnCollisionEnter(Collision collision) {
+
+        if (state != State.ALIVE) return;
+
         switch(collision.transform.tag) {
             case "Friendly":
                 Debug.Log("Friendly");
+                break;
+
+            case "Finished":
+                state = State.TRANSCENDING;
+                audioSource.clip = levelClearSound;
+                audioSource.Play();
+                Invoke("LoadSecondLevel", 1f);
                 break;
 
             case "Fuel":
@@ -60,8 +87,19 @@ public class Rocket : MonoBehaviour
                 break;
 
             default:
-                Debug.Log("Dead");
+                state = State.DYING;
+                audioSource.clip = deathSound;
+                audioSource.Play();
+                Invoke("LoadFirstLevel", 1f);
                 break;
         }
+    }
+
+    private void LoadSecondLevel() {
+        SceneManager.LoadScene(1);
+    }
+
+    private void LoadFirstLevel() {
+        SceneManager.LoadScene(0);
     }
 }
